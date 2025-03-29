ba04076b605ff956e26d823ac623a6b7d3db5181
Gabriel Matioli <gabematioli@outlook.com>
2025-03-29 11:24:22 -0700

    Model v1 created

--- model/main.py
+++ model/main.py
@@ -1,6 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from imblearn.over_sampling import SMOTE
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+
+## Data Preprocessing ##
 
 pd.set_option('display.max_columns', 30)
 df = pd.read_csv('../data/bank-additional-full.csv', sep=';')
@@ -18,3 +25,23 @@ ct = ColumnTransformer([('scaler', StandardScaler(), integer),
                         ('ordinal', OrdinalEncoder(), categorical)], remainder='passthrough')
 X_transformed = pd.DataFrame(ct.fit_transform(X), columns=ct.get_feature_names_out())
 
+sampler = SMOTE()
+X_sampled, y_sampled = sampler.fit_resample(X_transformed, y)
+
+## Model Training ##
+
+X_train, X_test, y_train, y_test = train_test_split(X_sampled, y_sampled, test_size=0.20, random_state=42)
+
+clf = GradientBoostingClassifier(learning_rate=0.8, n_estimators=200)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+## Model Testing ##
+
+clf_report = classification_report(y_test, y_pred)
+print('Classification report: \n')
+print(clf_report)
+
+cm = confusion_matrix(y_test, y_pred)
+ConfusionMatrixDisplay(cm).plot(cmap='Blues')
+plt.show()
